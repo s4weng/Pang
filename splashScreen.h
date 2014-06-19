74f5598ff78d50f4b478a1d99bf12a831ab46134
Steve <steve_weng@hotmail.ca>
2014-06-19 19:58:05 -0400

    added title screen files and missing stdafx headers

---
+++ splashScreen.h
@@ -0,0 +1,10 @@
+#ifndef __SPLASHSCREEN_H__
+#define __SPLASHSCREEN_H__
+
+class SplashScreen
+{
+public:
+	void Show(sf::RenderWindow& window);
+};
+
+#endif
---
+++ stdafx.h
@@ -0,0 +1,16 @@
+#ifndef __STDAFX_H__
+#define __STDAFX_H__
+
+#include <stdio.h>
+
+//SFML material
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include <iostream>
+#include <cassert>
+
+#endif
